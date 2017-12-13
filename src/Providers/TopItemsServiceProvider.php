683cb61c143bc56218bf2ce59a692843b8e8761a
Cristophor Hurduban <34269015+cristophor22@users.noreply.github.com>
2017-12-13 14:04:46 +0200

    plugin files added

---
+++ src/Providers/TopItemsServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TopItems\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class TopItemsServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+
+        $this->getApplication()->register( TopItemsRouteServiceProvider::class );
+
+    }
+
+}
